527ffc541353c3a5a19cedf24a016c166f32f0cd
Lachlan <Kermode>
2020-11-14 17:39:14 +0000

    fix home setup

--- config.fish
+++ config.fish
@@ -1,4 +1,17 @@
 set fish_greeting
-setxkbmap -layout us -option ctrl:nocaps
 xset r rate 220 40
-set -gx PATH $PATH ~/.local/bin/
+setxkbmap -option ctrl:nocaps
+alias tmux='env TERM=screen-256color-bce tmux'
+
+# GO paths
+set -gx PATH /usr/local/go/bin $PATH
+set -gx PATH /home/lachlan/go/bin $PATH
+
+# python
+set -gx PATH /home/lachlan/.local/bin $PATH
+
+# npm
+set -gx PATH /home/lachlan/.npm-global/bin $PATH
+
+# blender
+set -gx PATH /home/lachlan/lib/blender $PATH
--- homesetup.sh
+++ homesetup.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-xrandr --auto --output DP-1 --primary --mode 3840x2160 --scale 1x1 --above eDP-1
+xrandr --auto --output DP-1 --primary --mode 3840x2160 --scale 1x1 --right-of eDP-1
